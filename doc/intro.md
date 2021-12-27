a139bf71d383d21ded2e7cd71cf1f3b9e3b66d9a
Elijah Malaby <qwe12345678910@gmail.com>
2021-12-27 12:33:39 -0500

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to guac-testing
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/site/djei/guac_testing_test.clj
@@ -0,0 +1,7 @@
+(ns site.djei.guac-testing-test
+  (:require [clojure.test :refer :all]
+            [site.djei.guac-testing :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
